4364c192c238750efe9aaa89b03be13153ef79fb
ynzung <syj91105@sookmyung.ac.kr>
2024-11-06 22:36:47 +0900

    Add: account 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
+import Signup from "./pages/account/Signup";
+import Login from "./pages/account/Login";
 
 function App() {
   return (
