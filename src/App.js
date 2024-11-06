6052e6a639a42b716e5c99ecc2bceeeed275c697
ynzung <syj91105@sookmyung.ac.kr>
2024-11-06 22:06:05 +0900

    Feat: 회원가입 페이지 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <div>
-      <Login />
+      <Signup />
     </div>
   );
 }
